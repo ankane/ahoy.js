149b5077019a692788785f3aabdb5d4384b99f3e
Andrew Kane <acekane1@gmail.com>
2014-06-10 01:33:47 -0700

    Use UUIDs

--- ahoy.js
+++ ahoy.js
@@ -73,18 +73,12 @@
     }
   }
 
-  // https://github.com/klughammer/node-randomstring
+  // http://stackoverflow.com/a/2117523/1177228
   function generateId() {
-    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz';
-    var length = 32;
-    var string = '';
-
-    for (var i = 0; i < length; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      string += chars.substring(randomNumber, randomNumber + 1);
-    }
-
-    return string;
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
   }
 
   function saveEventQueue() {
