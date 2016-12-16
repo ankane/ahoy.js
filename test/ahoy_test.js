0ee7349f3915bf9cd584689141d913f4af2925bc
Richard Venneman <richardvenneman@me.com>
2016-12-16 22:37:12 +0100

    Setup before, after hooks

--- test/ahoy_test.js
+++ test/ahoy_test.js
@@ -1,11 +1,13 @@
 import test from 'tape';
 import Cookies from 'js-cookie';
 
-function setup() {
-  Cookies.set('ahoy_track', true);
-}
+const before = test;
+const after = test;
 
-setup();
+before('before', (t) => {
+  Cookies.set('ahoy_track', true);
+  t.end();
+})
 
 test('Defines ahoy', (t) => {
   t.plan(1);
@@ -22,3 +24,8 @@ test('Removes ahoy_track cookie when initializing', (t) => {
 
   t.equal(Cookies.get('ahoy_track'), undefined);
 });
+
+after('after', (t) => {
+  Cookies.remove('ahoy_track');
+  t.end();
+})
