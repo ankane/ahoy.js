bc01f585b1fdac4bbde9d3400e7eb99059af59a8
Andrew Kane <andrew@ankane.org>
2023-02-05 23:04:55 -0800

    Improved style [skip ci]

--- src/index.js
+++ src/index.js
@@ -145,7 +145,8 @@ function documentReady(callback) {
 // https://stackoverflow.com/a/2117523/1177228
 function generateId() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    const r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
 }
