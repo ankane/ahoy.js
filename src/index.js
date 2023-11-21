fbff69e1ad155a74bf30d63fa5edb9667e177a73
Fabio Cantoni <cover@revoc.net>
2023-11-21 23:15:12 +0100

    Improve generation of UUIDs

--- src/index.js
+++ src/index.js
@@ -144,11 +144,11 @@ function documentReady(callback) {
 
 // https://stackoverflow.com/a/2117523/1177228
 function generateId() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    const r = Math.random() * 16 | 0;
-    const v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
+  if (crypto.randomUUID) return crypto.randomUUID();
+
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
 }
 
 function saveEventQueue() {
